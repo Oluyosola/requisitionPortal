00718c6900000109c31e606219b6f3d7874629de
Oluyosola <afolabioluyosola@gmail.com>
2021-03-24 22:48:53 +0100

    Home page rebranded

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,17 +35,18 @@
     </div>
   </header><!-- End Header -->
    <!-- ======= Hero Section ======= -->
-   <section id="hero" class="d-flex align-items-center">
+   <section id="hero" class="d-flex align-items-center" >
     <div class="container">
       <h1>Requisition portal</h1>
-    </div>
-  </section><!-- End Hero -->
+    {{-- </div> --}}
+    
+  {{-- </section><!-- End Hero --> --}}
 
   <main id="main">
 
     <!-- ======= Why Us Section ======= -->
-    <section id="why-us" class="why-us">
-      <div class="container">
+    {{-- <section id="why-us" class="why-us"> --}}
+      <div class="why-us"  id="why-us">
 
         <div class="row">
           <div class="col-lg-4 d-flex align-items-stretch">
@@ -109,47 +110,22 @@
                 </div>
             </form>
             {{-- @extends('auth.login') --}}
-
             </div>
           </div>
-          <div class="col-lg-8 d-flex align-items-stretch">
-            <div class="icon-boxes d-flex flex-column justify-content-center">
-              <div class="row">
-                <div class="col-xl-4 d-flex align-items-stretch">
-                  <div class="icon-box mt-4 mt-xl-0">
-                    <i class="bx bx-receipt"></i>
-                    <h4>Corporis voluptates sit</h4>
-                    <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
-                  </div>
-                </div>
-                <div class="col-xl-4 d-flex align-items-stretch">
-                  <div class="icon-box mt-4 mt-xl-0">
-                    <i class="bx bx-cube-alt"></i>
-                    <h4>Ullamco laboris ladore pan</h4>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
-                  </div>
-                </div>
-                <div class="col-xl-4 d-flex align-items-stretch">
-                  <div class="icon-box mt-4 mt-xl-0">
-                    <i class="bx bx-images"></i>
-                    <h4>Labore consequatur</h4>
-                    <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
-                  </div>
-                </div>
-              </div>
-            </div> <!-- End .content-->
+</div>
+            </div>
           </div>
+        </main>
         </div>
-      </div>
-    </section><!-- End Why Us Section -->
-
+        </section>
+          
     
   <!-- ======= Footer ======= -->
   <footer id="footer">
 
     
           
-    <div class="container d-md-flex py-4">
+    <div class="container-fluid d-md-flex py-4">
 
       <div class="mr-md-auto text-center text-md-left">
         <div class="copyright">
