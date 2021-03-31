d6df51b76f48ca74b567cf598e1802038027ade9
Oluyosola <afolabioluyosola@gmail.com>
2021-03-31 09:44:28 +0100

    clevel controller index method commented out

--- app/Http/Controllers/CLevelController.php
+++ app/Http/Controllers/CLevelController.php
@@ -40,16 +40,16 @@ class ClevelController extends Controller
         // if($check1 || $check2 || $check3){
           
             // dd($user_id);
-        $clevel = Auth::user()->designation_type_id;
-        // dd($clevel);
-            $sql_query = "SELECT requisitions.id as id, requisitions.quantity as quantity, 
-        requisitions.description as description, users.name as user_name, categories.name as category_name, 
-        items.name as item_name FROM `requisitions` LEFT JOIN categories ON requisitions.category_id = categories.id
-        LEFT JOIN items ON requisitions.item_id = items.id
-        LEFT JOIN users ON requisitions.manager_id = users.id OR requisitions.user_id = users.id WHERE users.reporting_designation_type_id = $clevel
-        OR requisitions.is_clevel_approved = 1";
-        $results=  DB::select($sql_query);
-        return view('dashboards.c_level', compact('results'));
+        // $clevel = Auth::user()->designation_type_id;
+        // // dd($clevel);
+        //     $sql_query = "SELECT requisitions.id as id, requisitions.quantity as quantity, 
+        // requisitions.description as description, users.name as user_name, categories.name as category_name, 
+        // items.name as item_name FROM `requisitions` LEFT JOIN categories ON requisitions.category_id = categories.id
+        // LEFT JOIN items ON requisitions.item_id = items.id
+        // LEFT JOIN users ON requisitions.manager_id = users.id OR requisitions.user_id = users.id WHERE users.reporting_designation_type_id = $clevel
+        // OR requisitions.is_clevel_approved = 1";
+        // $results=  DB::select($sql_query);
+        // return view('dashboards.c_level', compact('results'));
         // dd( $results->user->location_id);
         // }
     }
